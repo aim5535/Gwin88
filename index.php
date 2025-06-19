efb7cae827df49740dd84b535921edd488a6ca3e
aim5535 <luckytest88vip@gmail.com>
2025-06-19 15:20:45 +0800

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data = json_decode(file_get_contents('data.json'), true);
-?>
-
-<img src="<?= $data['banner'] ?>" style="max-width:100%;">
-<h1><?= $data['title'] ?></h1>
-<a href="<?= $data['join_link'] ?>">立即加入</a>
